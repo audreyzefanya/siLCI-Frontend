3972591f7bb556b6443258d24553d4b4a5444bb5
rama.tridigdaya <rama.tridigdaya@ui.ac.id>
2024-05-02 15:50:47 +0700

    Rightful Admin Perusahaan Can See Add Button

--- src/pages/adminperusahaan/perusahaan/daftarbarang/index.js
+++ src/pages/adminperusahaan/perusahaan/daftarbarang/index.js
@@ -15,6 +15,8 @@ const DaftarBarangPerusahaan = (props) => {
     const [barang, setBarang] = useState([]);
     const navigateTo = useNavigate();
     const [searchText, setSearchText] = useState('');
+    const [buttonTambah, setButtonTambah] = useState(false)
+    const [userInfo, setUserInfo] = useState(null);
     var columns = [];
 
     if (barang) {
@@ -91,6 +93,22 @@ const DaftarBarangPerusahaan = (props) => {
         }
     }
     
+    useEffect(() => {
+        // Get user info from localStorage
+        const storedUserInfo = localStorage.getItem('userInfo');
+        if (storedUserInfo) {
+          setUserInfo(JSON.parse(storedUserInfo));
+        }
+      }, []);
+
+    useEffect(() => {
+        if(userInfo && perusahaan) {
+            if(userInfo.id === perusahaan.admin) {
+                setButtonTambah(true);
+            } 
+        }
+    }, [userInfo, perusahaan]);
+
     const handleDetailBarang = (barangId) => {
         navigateTo(`/admin-perusahaan/barang/${barangId}`);
     };
@@ -126,24 +144,26 @@ const DaftarBarangPerusahaan = (props) => {
                                         <div className="daftar-barang">
                                             Daftar Barang
                                         </div>
-                                        <Button
-                                            size="sm"
-                                            onClick={addBarangButton}
-                                            style={{
-                                            borderRadius: '20px',
-                                            backgroundColor: '#DA3732',
-                                            borderColor: '#DA3732',
-                                            color: 'white',
-                                            padding: '5px 15px',
-                                            fontSize: '1rem',
-                                            boxShadow: '0 4px 6px rgba(0,0,0,0.1)',
-                                            transition: 'transform 0.2s ease-in-out',
-                                            }}
-                                            onMouseOver={(e) => e.target.style.transform = 'scale(1.05)'}
-                                            onMouseOut={(e) => e.target.style.transform = 'scale(1)'}
-                                        >
-                                            + Tambah Barang
-                                        </Button>
+                                        {buttonTambah && (
+                                            <Button
+                                                size="sm"
+                                                onClick={addBarangButton}
+                                                style={{
+                                                borderRadius: '20px',
+                                                backgroundColor: '#DA3732',
+                                                borderColor: '#DA3732',
+                                                color: 'white',
+                                                padding: '5px 15px',
+                                                fontSize: '1rem',
+                                                boxShadow: '0 4px 6px rgba(0,0,0,0.1)',
+                                                transition: 'transform 0.2s ease-in-out',
+                                                }}
+                                                onMouseOver={(e) => e.target.style.transform = 'scale(1.05)'}
+                                                onMouseOut={(e) => e.target.style.transform = 'scale(1)'}
+                                            >
+                                                + Tambah Barang
+                                            </Button>
+                                        )}
                                     </div>
                                 }
                                 columns={columns}
