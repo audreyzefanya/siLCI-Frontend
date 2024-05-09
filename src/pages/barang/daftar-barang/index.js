f8687317a83286cedd527816dd86536bc31d1be6
Natasya Ashil Zhafirah <natasya.ashil@ui.ac.id>
2024-05-09 16:31:10 +0700

    Barang management (update add, update, detail, daftar barang)

--- src/pages/barang/daftar-barang/index.js
+++ src/pages/barang/daftar-barang/index.js
@@ -2,7 +2,6 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import DataTable from 'react-data-table-component';
 import { connect } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../../components/header';
@@ -27,8 +26,7 @@ const DaftarBarang = (props) => {
     fetchData();
   }, []);
 
-  const handleDetail = (id_barang, event) => {
-    event.stopPropagation();
+  const handleDetail = (id_barang) => {
     navigate(`/manager-operasional/barang/${id_barang}`);
   };
 
@@ -37,13 +35,17 @@ const DaftarBarang = (props) => {
     navigate(`/manager-operasional/add-barang`);
   };
 
+  const handleUpdateBarang = (id_barang, event) => {
+    event.stopPropagation();
+    navigate(`/manager-operasional/barang/update/${id_barang}`);
+  };
+
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
-  const handleUpdateBarang = (id_barang, event) => {
-    event.stopPropagation();
-    navigate(`/manager-operasional/barang/update/${id_barang}`);
+  const truncateText = (text, length) => {
+    return text.length > length ? text.substring(0, length) + "..." : text;
   };
 
   const filteredBarangData = barangData.filter(barang =>
@@ -51,96 +53,10 @@ const DaftarBarang = (props) => {
     barang.deskripsi.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  const columns = [
-    {
-      name: 'Nama Barang',
-      selector: row => row.nama,
-      sortable: true,
-      compact: true,
-      width: '35%',
-    },
-    {
-      name: 'Deskripsi',
-      selector: row => row.deskripsi,
-      sortable: true,
-      compact: true,
-      width: '35%',
-    },
-    {
-      cell: (row) => (
-        <div style={{ display: 'flex', justifyContent: 'start', gap: '8px' }}>
-          <Button
-            onClick={(e) => handleDetail(row.id, e)}
-            style={{
-              borderRadius: '5px',
-              backgroundColor: '#2C358C',
-              borderColor: '#2C358C',
-              color: 'white',
-              padding: '0.375rem 0.75rem',
-              fontSize: '0.8rem',
-            }}
-          >
-            View Details
-          </Button>
-          <Button
-            onClick={(e) => handleUpdateBarang(row.id, e)}
-            style={{
-              borderRadius: '5px',
-              backgroundColor: '#7C8FDC',
-              borderColor: '#7C8FDC',
-              color: 'white',
-              padding: '0.375rem 0.75rem',
-              fontSize: '0.8rem',
-            }}
-          >
-            Update Barang
-          </Button>
-        </div>
-      ),
-      ignoreRowClick: true,
-      allowOverflow: true,
-      button: true,
-      width: '30%',
-    },
-  ];
-
-  const customStyles = {
-    rows: {
-      style: {
-        minHeight: '72px',
-      },
-    },
-    headCells: {
-      style: {
-        color: '#202124',
-        fontSize: '16px',
-      },
-    },
-    cells: {
-      style: {
-        color: '#202124',
-        fontSize: '14px',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        paddingLeft: '16px',
-        paddingRight: '16px',
-      },
-    },
-    table: {
-      style: {
-        width: '100%',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      },
-    },
-  };
-
   return (
     <div className='flex w-screen h-screen'>
       <Sidebar currentNavigation={2.3} isExpand={props.isExpandSidebar} onClick={props.handleSidebarStatus}/>
-      <div className='flex flex-col w-full h-screen'>
+      <div className='w-full h-screen flex flex-col'>
         <Header title=''/>
         <div className="flex items-center text-3xl font-bold mb-10 ml-10 mt-8" style={{ color: '#000000' }}>
           <span style={{ marginRight: '20px' }}>Daftar Barang</span>
@@ -175,15 +91,45 @@ const DaftarBarang = (props) => {
             <FontAwesomeIcon icon={faSearch} style={{ position: 'absolute', top: '50%', left: '12px', transform: 'translateY(-50%)', color: '#A0AEC0', fontSize: '18px' }} />
           </Form.Group>
         </div>
-        <div style={{ padding: '20px' }}>
-          <DataTable
-            columns={columns}
-            data={filteredBarangData}
-            customStyles={customStyles}
-            highlightOnHover
-            pointerOnHover
-            pagination
-          />
+        <div className='no-scrollbar flex-1 overflow-y-auto bg-neutral20 py-6 px-8'>
+          <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {filteredBarangData.map(barang => (
+              <div key={barang.id} className="bg-white rounded-lg shadow-lg p-6 cursor-pointer transition-all duration-200 ease-in-out transform hover:-translate-y-1 hover:shadow-xl flex" style={{ borderLeft: '4px solid #2C358C' }}>
+                <img src={barang.foto || 'default_image_path.jpg'} alt={barang.nama} style={{ width: '100px', height: '100px', marginRight: '20px', objectFit: 'cover' }} />
+                <div>
+                  <h3 className="text-xl font-semibold mb-2" style={{ color: '#2C358C' }}>{barang.nama}</h3>
+                  <p className="mb-4 text-gray-700">{truncateText(barang.deskripsi, 20)}</p> 
+                  <div style={{ display: 'flex', gap: '8px' }}>
+                    <Button
+                      onClick={() => handleDetail(barang.id)}
+                      style={{
+                        borderRadius: '5px',
+                        backgroundColor: '#2C358C',
+                        borderColor: '#2C358C',
+                        color: 'white',
+                        padding: '7px 10px',
+                        fontSize: '0.875rem',
+                        transition: 'background-color 0.2s',
+                      }}
+                      onMouseOver={(e) => e.target.style.backgroundColor = '#DA3732'}
+                      onMouseOut={(e) => e.target.style.backgroundColor = '#2C358C'}
+                    > Lihat Detail </Button>
+                    <Button
+                      onClick={(e) => handleUpdateBarang(barang.id, e)}
+                      style={{
+                        borderRadius: '5px',
+                        backgroundColor: '#7C8FDC',
+                        borderColor: '#7C8FDC',
+                        color: 'white',
+                        padding: '7px 10px',
+                        fontSize: '0.875rem',
+                      }}
+                    > Ubah Barang </Button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
